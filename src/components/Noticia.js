c9e19b53cf899d9ed74c5dbd0341a39a322b37c5
Antonio Aguilar Herrera <antonio.herrera@live.com>
2019-07-29 17:28:53 -0500

    Imágen condicional

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const Noticia = ({noticia}) => {
-    const {urlToImage, url, title, description} = noticia;
+    const {urlToImage, url, title, description, source} = noticia;
+    const imagen = (urlToImage) ?
+        <div className="card-image">
+            <img src={urlToImage} alt={title} />
+            <span className="card-title">{source.name}</span>
+        </div>
+    : null;
+    
     return (
         <div className="col s12 m6 l4">
             <div className="card">
+                {imagen}
                 <div className="card-image">
                     <img src={urlToImage} alt={title} />
                 </div>
